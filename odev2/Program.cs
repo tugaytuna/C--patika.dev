424b8c3768fc701d7b3e9b5ade0d67553f9112d6
Tugay Tuna <tugaytuna@outlook.com.tr>
2022-09-06 16:28:11 +0300

    odev2

---
+++ odev2/Program.cs
@@ -0,0 +1,24 @@
+﻿
+//create new array
+int[] ciftrakamlar = new int[6];
+
+//adding values
+for (int i = 0, j = 0; i<=10; i++)
+{
+    if (i % 2 == 0)
+    {
+        ciftrakamlar[j] = i;
+         j++;
+    }
+}
+
+
+//read the array
+
+for (int i = 0; i<ciftrakamlar.Length;i++)
+{
+    Console.WriteLine(ciftrakamlar[i]);
+}
+
+
+
