bd6eff6421294167385722d868de9170e2f773a4
Tugay Tuna <tugaytuna@outlook.com.tr>
2022-09-06 16:02:22 +0300

    odev1

---
+++ odev1/Program.cs
@@ -0,0 +1,12 @@
+﻿int sayi1 = 10;
+int sayi2 = 20;
+int toplam = sayi1 + sayi2;
+
+string isim = "tugay";
+
+double gdp = 3.4;
+
+string ozet = isim + " " + gdp.ToString();
+
+
+Console.WriteLine(ozet);
