2aeebfd405fdc4cecbcc21daac15ae573dc9677c
Tugay Tuna <tugaytuna@outlook.com.tr>
2022-09-08 23:53:28 +0300

    proje2

--- proje2/ToDoApp/ToDoApp/Program.cs
+++ proje2/ToDoApp/ToDoApp/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("ToDo Project");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ToDoApp'e Hoş Geldiniz");
+
+
+        }
+    }
+}
