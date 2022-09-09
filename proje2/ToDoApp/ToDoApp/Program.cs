d39057c29296eebf719d20c988104d20209c1295
Tugay Tuna <tugaytuna@outlook.com.tr>
2022-09-09 12:14:17 +0300

    proje 2 update-not last version

--- proje2/ToDoApp/ToDoApp/Program.cs
+++ proje2/ToDoApp/ToDoApp/Program.cs
@@ -8,8 +8,29 @@ namespace ToDoApp
 {
     internal class Program
     {
+        class card
+        {
+            //Baslık
+            //Icerik
+            //Atanan Kisi(Takım üyelerişnden biri olmalı)
+            //Büyüklük(XS, S, M, L, XL)
+
+            string baslik { get; set; }
+            string icerik { get; set; }
+            string atanan_kisi { get; set; }
+            string buyukluk { get; set; }
+        }
+
         static void Main(string[] args)
         {
+            //Kart Ekle
+            //Kart Güncelle
+            //Kart Sil
+            //Kart Taşı
+            //Board Listeleme
+
+            card kart = new card();
+
             Console.WriteLine("ToDoApp'e Hoş Geldiniz");
 
 
